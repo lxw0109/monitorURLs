e427faa7af0163439c770d158363138e51bf1796
lxw <lxw.ucas@gmail.com>
2015-03-22 00:15:06 +0800

    debug OK

---
+++ debug.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#File: debug.sh
+#Author: lxw
+#Time: 2015-03-22
+#Usage: DEUBUG-Execute "python /home/lxw/Project/monitorURL/monitrURLs.py"
+
+#kill the PROCESSes that are not wanted to be alive.
+ps aux|grep "monitorURLs.py"|awk '{print $2}'|xargs kill
+
+#run th monitor.
+cd /home/lxw/monitorURL
+
+#Preparation:
+#update/create the following File/Dir.
+touch criterion
+touch accessErrorURLs
+[ -d Intermedia ] || mkdir Intermedia
+[ -d Intermedia_new ] || mkdir Intermedia_new
+
+rm ./Intermedia/*
+cp ./new_debug/* ./Intermedia/
+
+#Monitor:
+python ./monitorURLs.py >> ./monitorLog
