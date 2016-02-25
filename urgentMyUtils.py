74da8fffddf33844b0c62da6e29241516f4fc5ce
lxw0109 <lxw0109@gmail.com>
2016-02-25 14:38:06 +0800

    recover the ccAddrs in urgentMyUtils.py

--- urgentMyUtils.py
+++ urgentMyUtils.py
@@ -537,8 +537,7 @@ def sendEmail(subject, content):
         if "无信息更新" in content or "网站访问故障" in content: #NOT: if "无信息更新" or "网站访问故障" in content:
             ccAddrs = []
         else:
-            #ccAddrs = ["gengguanggang@cnnic.cn", "yanzhiwei@cnnic.cn", "wangcuicui@cnnic.cn"]#cList #["lxwin@foxmail.com"]
-            ccAddrs = []
+            ccAddrs = ["gengguanggang@cnnic.cn", "yanzhiwei@cnnic.cn", "wangcuicui@cnnic.cn"]#cList #["lxwin@foxmail.com"]
         bccAddrs = bList
 
         #toAddrs += ["gengguanggang@cnnic.cn", "yanzhiwei@cnnic.cn", "wangcuicui@cnnic.cn"]
